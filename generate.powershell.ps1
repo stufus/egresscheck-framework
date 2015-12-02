85cda436ce40036a0c4376dc28da8d551fae6776
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2015-12-02 18:13:49 +0000

    Added port string parsing code

--- generate.powershell.ps1
+++ generate.powershell.ps1
@@ -2,15 +2,32 @@
 
 function egresscheck {
     [CmdletBinding()]                                                                                                                                                                           
-    param([string]$ip, [int]$startPort = 1, [int]$endPort = 65535)
+    param([string]$ip, [string]$pr) 
 
-    for ($i = $startPort; $i -le $endPort; $i++)                                                                                                                                                
-    {
-		_tcp -ip "$ip" -port $i
-		_udp -ip "$ip" -port $i
+    $pr_split = $portrange -split ','
+    $ports = @()
+    foreach ($p in $pr_split) {
+        if ($p -match '^[0-9]+-[0-9]+$') {
+            $prange = $p -split '-'
+            for ($c = [convert]::ToInt32($prange[0]);$c -le [convert]::ToInt32($prange[1]);$c++) {
+                $ports += $c
+            }
+        } elseif ($p -match '^[0-9]+$') {
+            $ports += $p
+        } else {
+            #Error in port definition
+            return
+        }
     }
 
-    Write-Output "Finished"     
+    $ports
+
+    foreach ($eachport in $ports) {
+		Write-Output "_tcp -ip $ip -port $eachport"
+		Write-Output "_udp -ip $ip -port $eachport"
+    }
+
+    #Write-Output "Finished"     
 }
 
 function _tcp {
@@ -39,4 +56,6 @@ function _udp {
 	catch { }  
 }
 
-egresscheck -ip "127.0.0.1" -startPort 1 -endPort 65535
+$ip = "127.0.0.1"
+$portrange = "20-30,40,50"
+egresscheck -ip $ip -pr $portrange
